894cf5a20792c6cc928a0935720d2bd609f527fb
SeGe Jung <sjung332@gatech.edu>
2020-01-31 11:30:28 -0500

    changed activity name

---
+++ Assignment4/SDPEncryptor/app/src/main/java/edu/gatech/seclass/sdpencryptor/SDPEncryptorActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.seclass.sdpencryptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SDPEncryptorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
