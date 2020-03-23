a558c4eba05da4148f6f98111b286837b71d385d
Jung, SeGe <sjung332@gatech.edu>
2020-03-22 21:07:12 -0400

    Update TestCoverageClass.java

--- Assignment7/src/edu/gatech/seclass/TestCoverageClass.java
+++ Assignment7/src/edu/gatech/seclass/TestCoverageClass.java
@@ -1,13 +1,58 @@
 package edu.gatech.seclass;
-import java.lang.ArithmeticException;
 
 
 public class TestCoverageClass {
 
-    public class testCoverageMethod1 extends ArithmeticException {
+    public void testCoverageMethod1 () {
 
     }
 
+    public void testCoverageMethod2 () {
+
+    }
+
+    public void testCoverageMethod3 () {
+
+    }
+
+    public void testCoverageMethod4 () {
+
+    }
+
+    public boolean testCoverageMethod5 (boolean a, boolean b) {
+        int x = 3;
+        int y = 1;
+        if(a)
+            x += y;
+        else
+            y = y*x;
+        if(b)
+            y -= x;
+        else
+            y -= 1;
+        return ((x/y)>= 0);
+    }
+
+
+// ================
+//
+    // Fill in column “output” with T, F, or E:
+    //
+// | a | b |output|
+// ================
+// | T | T |      |
+// | T | F |      |
+// | F | T |      |
+// | F | F |      |
+// ================
+//
+// Fill in the blanks in the following sentences with
+// “NEVER”, “SOMETIMES” or “ALWAYS”:
+//
+// Test suites with 100% statement coverage _____ reveal the fault in this method.
+// Test suites with 100% branch coverage ______ reveal the fault in this method.
+// Test suites with 100% path coverage ______ reveal the fault in this method.
+// ================
 
 }
 
